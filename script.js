8996d99048f476d0117b5165b8147a9d64917694
mujeeb <muzeeb100@yahoo.co.in>
2022-03-25 00:32:45 +0530

    guessing number

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '✨Correct Number';
+
+document.querySelector('.number').textContent=13;
+document.querySelector('.score').textContent=10;
+
+document.querySelector('.guess').value=23;
+console.log(document.querySelector('.guess').value);
+*/
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '✨ Correct Number ✨';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the Game!';
+    }
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
